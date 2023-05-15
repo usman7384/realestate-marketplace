d9adbd78088e3b2031712e23c14d18ef58cb49af
tayyba <95464545+TayybaSaveera@users.noreply.github.com>
2023-05-16 00:05:00 +0500

    message routes added

--- controllers/messages.js
+++ controllers/messages.js
@@ -93,5 +93,122 @@ messageRouter.get("/:sender/:reciever", async (request, response) => {
   }
 });
 
+//send a simple message
+messageRouter.post("/simple", async (request, response) => {
+  try {
+    const { sender, reciever, content } = request.body;
+    const senderUser = await User.findById(sender);
+    const recieverUser = await User.findById(reciever);
+    const message = new Message({
+      sender: senderUser._id,
+      receiver: recieverUser._id,
+      content,
+      dateTime: new Date(),
+    });
+    const savedMessage = await message.save();
+    response.json(savedMessage);
+  } catch (error) {
+    response.status(400).end();
+  }
+});
+//send a message with attachments
+messageRouter.post(
+  "/attachment",
+  upload.array("attachments"),
+  async (request, response) => {
+    try {
+      const { sender, reciever, content } = request.body;
+      const senderUser = await User.findById(sender);
+      const recieverUser = await User.findById(reciever);
+      if (!senderUser || !recieverUser) {
+        return response
+          .status(400)
+          .json({ error: "Invalid sender or reciever." });
+      }
+      const attachments = request.files.map((file) =>
+        file.path.replace(/\\/g, "/"),
+      );
+      const message = new Message({
+        sender: senderUser._id,
+        receiver: recieverUser._id,
+        content,
+        attachments,
+        dateTime: new Date(),
+      });
+      const savedMessage = await message.save();
+      response.json(savedMessage);
+    } catch (error) {
+      console.log(error);
+      response.status(500).json({ error: "Failed to send message." });
+    }
+  },
+);
 
+//search in messages
+
+messageRouter.get("/search", async (request, response) => {
+  try {
+    const { keyword } = request.query;
+    const messages = await Message.find({
+      content: { $regex: keyword, $options: "i" },
+    });
+    response.json(messages);
+  } catch (error) {
+    response.status(400).end();
+  }
+});
+//archuive a message
+messageRouter.put("/:messageId/archive", async (request, response) => {
+  try {
+    const { messageId } = request.params;
+    const message = await Message.findByIdAndUpdate(
+      messageId,
+      { isArchived: true },
+      { new: true },
+    );
+    response.json(message);
+  } catch (error) {
+    response.status(400).end();
+  }
+});
+
+//unarchive a message
+
+messageRouter.put("/:messageId/unarchive", async (request, response) => {
+  try {
+    const { messageId } = request.params;
+    const message = await Message.findByIdAndUpdate(
+      messageId,
+      { isArchived: false },
+      { new: true },
+    );
+    response.json(message);
+  } catch (error) {
+    response.status(400).end();
+  }
+});
+//mark as read
+messageRouter.put("/:messageId/read", async (request, response) => {
+  try {
+    const { messageId } = request.params;
+    const message = await Message.findByIdAndUpdate(
+      messageId,
+      { isRead: true },
+      { new: true },
+    );
+    response.json(message);
+  } catch (error) {
+    response.status(400).end();
+  }
+});
+//delete a message
+messageRouter.delete("/:messageId", async (request, response) => {
+  try {
+    const { messageId } = request.params;
+    const message = await Message.findByIdAndDelete(messageId);
+    response.status(204).end();
+  } catch (error) {
+    response.status(400).end();
+  }
+});
 module.exports = messageRouter;
